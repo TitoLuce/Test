00ab77253953356b2b9b4b615f33dd6fc8e6cf94
Weedzard <arndslol@gmail.com>
2020-02-19 11:58:55 +0100

    Update Source.cpp

--- SDL Intro/Source.cpp
+++ SDL Intro/Source.cpp
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <iostream>
+#include "SDL/include/SDL.h"
+#pragma comment(lib,"SDL/x86/SDL2.lib")
+#pragma comment(lib, "SDL/x86/SDL2main.lib")
 
-int main()
+int main(int argc, char* argv[])
 {
 	std::cout << "Sup niggas" << std::endl;
 
