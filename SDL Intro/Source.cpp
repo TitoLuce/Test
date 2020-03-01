ad66b5f707753a07ae7f15635654ea33c0c6bbf3
Weedzard <59049803+TitoLuce@users.noreply.github.com>
2020-03-01 19:06:27 +0100

    EH

--- SDL Intro/Source.cpp
+++ SDL Intro/Source.cpp
@@ -6,78 +6,38 @@
 #pragma comment(lib, "SDL/x86/SDL2main.lib")
 #pragma comment(lib,"SDL_Image/x86/SDL2_image.lib")
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 SDL_Rect rect = { 0,0,100,100 };
 SDL_Rect Laser[10] = { rect.x,rect.y,50,25 };
-SDL_Renderer 
 int laserCount = 0;
 SDL_Event event;
 bool quit = false;
 
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+
 int main(int argc, char* argv[])  //Cuantos argumentos hemos recibido en el exe? Array con la lista de argumentos   (Argument count/Argument variables)
 {
-	SDL_Window* window = NULL;
-	SDL_Surface* screenSurface = NULL;
-	SDL_Surface* image = NULL;
+	SDL_Surface* NEPU = SDL_LoadBMP("NEPU.bmp");
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		window = SDL_CreateWindow("Ur mom", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 900, 900, SDL_WINDOW_SHOWN);
-	}
+	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	screenSurface = SDL_GetWindowSurface(window);
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xBB, 0xFF, 0xFF));
+	//NEPU = SDL_LoadBMP("NEPU");
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		screenSurface = SDL_GetWindowSurface(window);
-		SDL_Surface* surf =IMG_Load("NEPU.png");
-		SDL_Texture* text=SDL_CreateTextureFromSurface()
-		while (!quit)
-		{
-			while (SDL_PollEvent(&event) != 0)
-			{
-				if (event.type == SDL_QUIT)
-				{
-					quit = true;
-				}
-				else if (event.type == SDL_KEYDOWN)
-				{
-					switch(event.key.keysym.sym)
-					{
-					case SDLK_ESCAPE:
-						quit = true;
-						break;
-					case SDLK_UP:
-						rect.y--;
-						break;
-					case SDLK_DOWN:
-						rect.y++;
-						break;
-					case SDLK_LEFT:
-						rect.x--;
-						break;
-					case SDLK_RIGHT:
-						rect.x++;
-						break;
-					case SDLK_SPACE:
-						SDL_FillRect(screenSurface, &Laser[laserCount], SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
-						break;
-					}
-				}
-			}
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0xF7, 0xFF));
-			//SDL_FillRect(screenSurface, &rect, SDL_MapRGB(screenSurface->format, 0xFF, 0x00, 0x3E));
-			SDL_UpdateWindowSurface(window);
 
-		}
+	SDL_BlitSurface(NEPU, NULL, screenSurface, NULL);
+	while (NEPU!=NULL)
+	{
+
+		 
+
+		SDL_UpdateWindowSurface(window);
+	}
 	
-    }
-	
-	
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
